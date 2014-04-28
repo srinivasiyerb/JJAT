1f6991e98f2d2f400fa618e7e896ff09e28b52ed
srinivas <srinivasiyerb@gmail.com>
2014-04-28 11:21:02 +0530

    Update PlanAndEstimation.java

--- E2EMF_AT/src/test/java/com/rl/E2EMF/PlanAndEstimation.java
+++ E2EMF_AT/src/test/java/com/rl/E2EMF/PlanAndEstimation.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.By;
 public class PlanAndEstimation extends MainDriver {
 
 	public static void pe(){
-		driver.findElement(By.xpath("//*[@id='left-panel']/nav/ul/li[4]/a")).click();
-		driver.findElement(By.xpath("//*[@id='left-panel']/nav/ul/li[4]/ul/li[1]/a")).click();
-		driver.findElement(By.xpath("//*[@id='left-panel']/nav/ul/li[4]/ul/li[1]/ul/li[1]/a")).click();
-		driver.findElement(By.xpath("//*[@id='left-panel']/nav/ul/li[4]/ul/li[1]/ul/li[3]/a")).click();
+		driver.findElement(By.xpath("//aside[@id='left-panel']/nav/ul/li[4]/a/span")).click();
+		driver.findElement(By.xpath("//a[contains(@href, '#')][3]")).click();
+		driver.findElement(By.xpath("//a[contains(@href, 'ajax/e2e_pom.html')]")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'Outsourced Manufacturing')]")).click();
 		
-		driver.findElement(By.xpath("//*[@id='left-panel']/nav/ul/li[4]/ul/li[2]/a")).click();
+	/*	driver.findElement(By.xpath("//*[@id='left-panel']/nav/ul/li[4]/ul/li[2]/a")).click();
 		driver.findElement(By.xpath("//*[@id='left-panel']/nav/ul/li[4]/ul/li[2]/ul/li/a")).click();
-	}
+	*/}
 
 }
