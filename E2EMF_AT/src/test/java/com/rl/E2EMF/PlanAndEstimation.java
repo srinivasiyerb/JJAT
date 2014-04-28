1a00d3775dec980cddc0886096270174766dfe91
srinivas <srinivasiyerb@gmail.com>
2014-04-28 13:15:22 +0530

    Update PlanAndEstimation.java

--- E2EMF_AT/src/test/java/com/rl/E2EMF/PlanAndEstimation.java
+++ E2EMF_AT/src/test/java/com/rl/E2EMF/PlanAndEstimation.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 public class PlanAndEstimation extends MainDriver {
 
 	public static void pe(){
-			driver.findElement(By.xpath("//div[@id='hide-menu']/span/a").click();
+			driver.findElement(By.xpath("//div[@id='hide-menu']/span/a")).click();
 		
 	/*	driver.findElement(By.xpath("//*[@id='left-panel']/nav/ul/li[4]/ul/li[2]/a")).click();
 		driver.findElement(By.xpath("//*[@id='left-panel']/nav/ul/li[4]/ul/li[2]/ul/li/a")).click();
