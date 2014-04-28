7cb5e70c9fe5e28b746fbabdabc84d39a3dd25ea
srinivas <srinivasiyerb@gmail.com>
2014-04-28 13:27:32 +0530

    Update FlowDiagrams.java

--- E2EMF_AT/src/test/java/com/rl/E2EMF/FlowDiagrams.java
+++ E2EMF_AT/src/test/java/com/rl/E2EMF/FlowDiagrams.java
@@ -7,11 +7,11 @@ import org.openqa.selenium.By;
 public class FlowDiagrams extends MainDriver {
 	
 	public static void fd(){
-		driver.findElement(By.xpath("//aside[@id='left-panel']/nav/ul/li[1]/ul/li[2]/a")).click();
-		driver.findElement(By.xpath("//aside[@id='left-panel']/nav/ul/li[1]/ul/li[3]/a")).click();
+		driver.findElement(By.xpath("//aside[@id='left-panel']/nav/ul/li/a/i")).click();
+		driver.findElement(By.xpath("//aside[@id='left-panel']/nav/ul/li/ul/li[3]/a")).click();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("//aside[@id='left-panel']/nav/ul/li[1]/ul/li[4]/a")).click();
+	/*	driver.findElement(By.xpath("//aside[@id='left-panel']/nav/ul/li[1]/ul/li[4]/a")).click();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-	}
+	*/ }
 	
 }
