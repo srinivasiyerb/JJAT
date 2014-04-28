4542048b6f042de7b5ad43fef7e5c7f2da2fc4a0
srinivas <srinivasiyerb@gmail.com>
2014-04-28 11:15:57 +0530

    Update Search.java

--- E2EMF_AT/src/test/java/com/rl/E2EMF/Search.java
+++ E2EMF_AT/src/test/java/com/rl/E2EMF/Search.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 public class Search extends MainDriver {
 
 	 public static void search(){
-		 	driver.findElement(By.xpath("//aside[@id='left-panel']/nav/ul/li[3]/a")).click();
+		 	driver.findElement(By.xpath("//aside[@id='left-panel']/nav/ul/li[3]/a/span")).click();
 			driver.findElement(By.id("searchterm")).sendKeys("Sales orders Details");
 			driver.findElement(By.id("search")).click();
 			driver.manage().timeouts().implicitlyWait(160, TimeUnit.SECONDS);
